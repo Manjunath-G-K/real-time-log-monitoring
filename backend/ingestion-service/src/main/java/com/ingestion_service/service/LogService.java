7b75771eb9804a628f7e3a68d881ad072819feb2
Manjunath G K <manjunathgk146@gmail.com>
2026-01-28 18:18:23 +0530

    Added email and phone masking before log encryption

--- backend/ingestion-service/src/main/java/com/ingestion_service/service/LogService.java
+++ backend/ingestion-service/src/main/java/com/ingestion_service/service/LogService.java
@@ -2,11 +2,23 @@ package com.ingestion_service.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class LogService {
 
+    private static final Pattern EMAIL_PATTERN =
+            Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
+
+    private static final Pattern PHONE_PATTERN =
+            Pattern.compile("\\b\\d{10}\\b");
+
     public String processLog(String service, String message) {
-        String encryptedMessage = encrypt(message);
+
+        String maskedMessage = maskSensitiveData(message);
+        String encryptedMessage = encrypt(maskedMessage);
 
         System.out.println("Service: " + service);
         System.out.println("Encrypted Message: " + encryptedMessage);
@@ -14,8 +26,39 @@ public class LogService {
         return encryptedMessage;
     }
 
+    private String maskSensitiveData(String message) {
+        message = maskEmail(message);
+        message = maskPhone(message);
+        return message;
+    }
+
+    private String maskEmail(String message) {
+        Matcher matcher = EMAIL_PATTERN.matcher(message);
+        StringBuffer result = new StringBuffer();
+
+        while (matcher.find()) {
+            String email = matcher.group();
+            String maskedEmail = "***@" + email.substring(email.indexOf("@") + 1);
+            matcher.appendReplacement(result, maskedEmail);
+        }
+        matcher.appendTail(result);
+        return result.toString();
+    }
+
+    private String maskPhone(String message) {
+        Matcher matcher = PHONE_PATTERN.matcher(message);
+        StringBuffer result = new StringBuffer();
+
+        while (matcher.find()) {
+            String phone = matcher.group();
+            String maskedPhone = "******" + phone.substring(6);
+            matcher.appendReplacement(result, maskedPhone);
+        }
+        matcher.appendTail(result);
+        return result.toString();
+    }
+
     private String encrypt(String message) {
-        // Simple encryption for learning (Base64  )
-        return java.util.Base64.getEncoder().encodeToString(message.getBytes());
+        return Base64.getEncoder().encodeToString(message.getBytes());
     }
 }
