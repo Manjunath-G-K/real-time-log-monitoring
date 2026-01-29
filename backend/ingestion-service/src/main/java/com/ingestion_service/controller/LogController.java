f3d742cc8d6edc8376e03c9db6359332f3e307cf
Manjunath G K <manjunathgk146@gmail.com>
2026-01-29 10:05:46 +0530

    added endpoint for recentlogs

--- backend/ingestion-service/src/main/java/com/ingestion_service/controller/LogController.java
+++ backend/ingestion-service/src/main/java/com/ingestion_service/controller/LogController.java
@@ -22,15 +22,23 @@ import com.ingestion_service.model.LogRequest;
 import com.ingestion_service.service.LogService;
 import org.springframework.web.bind.annotation.*;
 
+import com.ingestion_service.store.InMemoryLogStore;
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/logs")
 public class LogController {
 
     private final LogService logService;
 
-    public LogController(LogService logService) {
+    private final InMemoryLogStore logStore;
+
+    public LogController(LogService logService, InMemoryLogStore logStore) {
         this.logService = logService;
+        this.logStore = logStore;
     }
+
 
     @PostMapping
     public String receiveLog(@RequestBody LogRequest logRequest) {
@@ -40,4 +48,10 @@ public class LogController {
         );
         return "Log received and processed";
     }
+
+    @GetMapping
+    public List<String> getRecentLogs() {
+        return logStore.getRecentLogs();
+    }
+
 }
