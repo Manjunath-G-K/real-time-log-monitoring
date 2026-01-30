cbe895633286f9d9bda6cb5f08cfa21fc2967bd6
Manjunath G K <manjunathgk146@gmail.com>
2026-01-30 11:25:09 +0530

    Day 6: Add WebSocket-based real-time log streaming

---
+++ backend/ingestion-service/src/main/java/com/ingestion_service/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.ingestion_service.config;
+
+import com.ingestion_service.websocket.LogWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final LogWebSocketHandler logWebSocketHandler;
+
+    public WebSocketConfig( LogWebSocketHandler logWebSocketHandler) {
+        this.logWebSocketHandler = logWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(logWebSocketHandler, "/ws/logs")
+                .setAllowedOrigins("*");
+    }
+}
---
+++ backend/ingestion-service/src/main/java/com/ingestion_service/websocket/LogWebSocketHandler.java
@@ -0,0 +1,36 @@
+package com.ingestion_service.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+public class LogWebSocketHandler extends TextWebSocketHandler {
+
+    private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+
+    public void broadcast(String message) {
+        for (WebSocketSession session : sessions) {
+            try {
+                if (session.isOpen()) {
+                    session.sendMessage(new TextMessage(message));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
